6c5901f6f6c68ebd664a19e073164f15db5d19a5
Roland Sadowski <szabla@gmail.com>
2025-05-13 19:05:24 +0100

    WIP: remove assertEquals() and force failure
    Tests were hanging on that assert during GeometryTest. The underlying
    method of comparing .ogex files needs to be replaced with something
    more robust than line-by-line comparison.

--- tests/TestUtils.py
+++ tests/TestUtils.py
@@ -29,10 +29,8 @@ class OgexExporterTest(unittest.TestCase):
         :param filename: name of the file to read the contents of
         :return: Contents of the file with given filename
         """
-        file = open(filename)
-        contents = file.readlines()
-        file.close()
-
+        with open(filename) as f:
+            contents = f.readlines()
         return contents
 
     def assertFilesEqual(self, test_filename, expected_filename):
@@ -41,4 +39,12 @@ class OgexExporterTest(unittest.TestCase):
         :param test_filename: name of the file to test
         :param expected_filename: name of the file containing expected content
         """
-        self.assertEqual(self.readContents(test_filename), self.readContents(expected_filename))
+        test_file_contents = self.readContents(test_filename)
+        expected_file_contents = self.readContents(expected_filename)
+        self.assertEqual(len(test_file_contents), len(expected_file_contents))
+        # NOTE(rosado): assertEquals hang forever here, possibly because unitttest
+        # attempts to make a diff? Haven't debugged it - will replace line by line
+        # comparisons anyway...
+        self.fail('Failing on purpose, tests need updating') 
+        
+
